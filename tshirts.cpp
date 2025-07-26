9705fa760d89db62fde4af5f97703c96ae681feb
uwp1kor <Uppaluru.Vasundara@in.bosch.com>
2025-07-26 11:24:12 +0530

    fix Tshirt size error

--- tshirts.cpp
+++ tshirts.cpp
@@ -5,9 +5,9 @@ char shoulderSize(int cms) {
     char sizeName = '\0';
     if(cms < 38) {
         sizeName = 'S';
-    } else if(cms > 38 && cms < 42) {
+    } else if(cms < 42) {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else {
         sizeName = 'L';
     }
     return sizeName;
@@ -15,7 +15,6 @@ char shoulderSize(int cms) {
 
 void testTshirtSize() {
     std::cout << "\nTshirt size test\n";
-    assert(shoulderSize(5) == 'error');
     assert(shoulderSize(37) == 'S');
     assert(shoulderSize(38) == 'M');
     assert(shoulderSize(40) == 'M');
